66e8861fb4a792d95c8e147d519230bfdb93998a
Ilya Drabenia <drobenyai@tut.by>
2016-05-04 18:18:55 -0700

    Correct code formatting

--- photoresistor.c
+++ photoresistor.c
@@ -9,59 +9,75 @@ typedef unsigned int uint;
 // code from http://www.sunfounder.com/learn/Universal-Starter-Kit-for-Raspberry-Pi/lesson-10-photoresistor-universal-kit-for-pi.html
 uchar get_ADC_Result(void)
 {
-//10:CH0
-//11:CH1
-uchar i;
-uchar dat1=0, dat2=0;
-digitalWrite(ADC_CS, 0);
-digitalWrite(ADC_CLK,0);
-digitalWrite(ADC_DIO,1); delayMicroseconds(2);
-digitalWrite(ADC_CLK,1); delayMicroseconds(2);
-digitalWrite(ADC_CLK,0);
-digitalWrite(ADC_DIO,1); delayMicroseconds(2); //CH0 10
-digitalWrite(ADC_CLK,1); delayMicroseconds(2);
-digitalWrite(ADC_CLK,0);
-digitalWrite(ADC_DIO,0); delayMicroseconds(2); //CH0 0
+  //10:CH0
+  //11:CH1
+  uchar i;
+  uchar dat1 = 0, dat2 = 0;
+  digitalWrite(ADC_CS, 0);
+  digitalWrite(ADC_CLK, 0);
+  digitalWrite(ADC_DIO, 1); delayMicroseconds(2);
+  digitalWrite(ADC_CLK, 1); delayMicroseconds(2);
+  digitalWrite(ADC_CLK, 0);
+  digitalWrite(ADC_DIO, 1); delayMicroseconds(2); //CH0 10
+  digitalWrite(ADC_CLK, 1); delayMicroseconds(2);
+  digitalWrite(ADC_CLK, 0);
+  digitalWrite(ADC_DIO, 0); delayMicroseconds(2); //CH0 0
 
-digitalWrite(ADC_CLK,1); 
-digitalWrite(ADC_DIO,1); delayMicroseconds(2);
-digitalWrite(ADC_CLK,0); 
-digitalWrite(ADC_DIO,1); delayMicroseconds(2);
+  digitalWrite(ADC_CLK, 1); 
+  digitalWrite(ADC_DIO, 1); delayMicroseconds(2);
+  digitalWrite(ADC_CLK, 0); 
+  digitalWrite(ADC_DIO, 1); delayMicroseconds(2);
 
-for(i=0;i<8;i++)
-{
-digitalWrite(ADC_CLK,1); delayMicroseconds(2);
-digitalWrite(ADC_CLK,0); delayMicroseconds(2);
-pinMode(ADC_DIO, INPUT);
-dat1=dat1<<1 | digitalRead(ADC_DIO);
+  for (i = 0; i < 8; i++)
+  {
+    digitalWrite(ADC_CLK, 1); 
+    delayMicroseconds(2);
+    
+    digitalWrite(ADC_CLK, 0); 
+    delayMicroseconds(2);
+    
+    pinMode(ADC_DIO, INPUT);
+    dat1 = dat1 << 1 | digitalRead(ADC_DIO);
+  }
+
+  for (i=0;i<8;i++)
+  {
+    dat2 = dat2 | ((uchar)(digitalRead(ADC_DIO)) << i);
+    
+    digitalWrite(ADC_CLK, 1); 
+    delayMicroseconds(2);
+    
+    digitalWrite(ADC_CLK, 0); 
+    delayMicroseconds(2);
+  }
+  
+  digitalWrite(ADC_CS, 1);
+  pinMode(ADC_DIO, OUTPUT);
+
+  return (dat1 == dat2) ? dat1 : 0;
 }
 
-for(i=0;i<8;i++)
-{
-dat2 = dat2 | ((uchar)(digitalRead(ADC_DIO))<<i);
-digitalWrite(ADC_CLK,1); delayMicroseconds(2);
-digitalWrite(ADC_CLK,0); delayMicroseconds(2);
-}
-digitalWrite(ADC_CS,1);
-pinMode(ADC_DIO, OUTPUT);
-
-return(dat1==dat2) ? dat1 : 0;
-}
 int main(void)
 {
-uchar adcVal;
-if(wiringPiSetup() == -1){
-printf("setup wiringPi failed !");
-return 1; 
-}
-pinMode(ADC_CS, OUTPUT);
-pinMode(ADC_CLK, OUTPUT);
-while(1){
-pinMode(ADC_DIO, OUTPUT);
-adcVal = get_ADC_Result();
-printf("adcval : %d\n",adcVal-80);
+  uchar adcVal;
+  if (wiringPiSetup() == -1) 
+  {
+    printf("setup wiringPi failed !");
+    return 1; 
+  }
+  
+  pinMode(ADC_CS, OUTPUT);
+  pinMode(ADC_CLK, OUTPUT);
+  
+  while (1) 
+  {
+    pinMode(ADC_DIO, OUTPUT);
+    adcVal = get_ADC_Result();
+    
+    printf("adcval : %d\n", adcVal - 80);
 
-delay(400);
-}
-return 0;
+    delay(400);
+  }
+  
+  return 0;
 }
